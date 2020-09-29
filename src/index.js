0298f2b69e5d898f0098ab67f638c7ab68472855
AnnaDavydenko <annadavydenko2544737@gmail.com>
2020-09-29 13:14:08 +0300

    typical arrays problems solving

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length > 0) {
+        return array.sort((a, b) => a - b)[0];
+    } else {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length > 0) {
+        return array.sort((a, b) => b - a)[0];
+    } else {
+        return 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length > 0) {
+        let sum = array.reduce((sum, item) => {
+            return sum + item;
+        }, 0);
+        return sum / array.length;
+    } else {
+        return 0;
+    }
 }
